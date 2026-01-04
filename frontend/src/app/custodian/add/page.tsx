24b9f4bbd8c799d457ffcc8268e416650e30e142
Benny <76574420+bennytrieu@users.noreply.github.com>
2026-01-03 17:58:00 -1000

    added changes on frontend made from add-custodian-logic-2 branch onto this branch

--- frontend/src/app/custodian/add/page.tsx
+++ frontend/src/app/custodian/add/page.tsx
@@ -1,23 +1,26 @@
 "use client";
 
 import { useState } from "react"
-import type { Supervisor } from "@/lib/types"
+import type { Custodian } from "@/lib/types"
 import { useToast } from "@/app/components/Toast"
 import axios from "axios"
 
 export default function addCustodian() {
 
-    const [janitor, setJanitor] = useState<Supervisor>({
-        name: null,
+    const [custodian, setCustodian] = useState<Custodian>({
+        name: "",
         id: undefined,
+        role: undefined,
+        boss_id: undefined
     });
+    const [showDropdown, setShowDropdown] = useState(false);
 
     const { showToast } = useToast()
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        if (!janitor.name || !janitor.id) {
+        if (!custodian.name || !custodian.id) {
             showToast("Please fill out required information", "fail");
             return;
         }
@@ -26,12 +29,12 @@ export default function addCustodian() {
             const { data } = await axios.post(
                 "http://localhost:8000/api/supervisors/",
                 {
-                    name: janitor.name,
-                    id: janitor.id
+                    name: custodian.name,
+                    id: custodian.id
                 },
             );
-            console.log(janitor)
-            showToast(`Successfully added ${janitor.name}`, 'success');
+            console.log(custodian)
+            showToast(`Successfully added ${custodian.name}`, 'success');
         } catch (error) {
             console.error(error);
             showToast(`${error}`, 'fail');
@@ -45,7 +48,7 @@ export default function addCustodian() {
         //             headers: {
         //                 "Content-Type": "application/json",
         //             },
-        //             body: JSON.stringify(janitor),
+        //             body: JSON.stringify(custodian),
         //         });
 
         //         if (!response.ok) {
@@ -72,16 +75,48 @@ export default function addCustodian() {
                         <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                             <div>
                                 <label htmlFor="firstname" className="block mb-2 text-sm font-medium text-slate-800">Full Name</label>
-                                <input type="text" name="firstname" id="firstname" value={janitor.name || ""} onChange={(e) => setJanitor({ ...janitor, name: e.target.value })} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Full Name" />
+                                <input type="text" name="firstname" id="firstname" value={custodian.name || ""} onChange={(e) => setCustodian({ ...custodian, name: e.target.value === "" ? null : e.target.value })} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Full Name" />
                             </div>
                             <div>
                                 <label htmlFor="id" className="block mb-2 text-sm font-medium text-slate-800">ID Number</label>
-                                <input type="text" name="id" id="id" value={janitor.id || ""} onChange={(e) => setJanitor({ ...janitor, id: Number(e.target.value) })} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="ID Number" />
+                                <input type="text" name="id" id="id" value={custodian.id || ""} onChange={(e) => setCustodian({ ...custodian, id: Number(e.target.value) })} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="ID Number" />
                             </div>
                             <div>
-                                <label htmlFor="role" className="block mb-2 text-sm font-medium text-slate-800">Role</label>
-                                <input type="text" name="role" id="role" value="Supervisor" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Janitor Role" readOnly />
+                                <label htmlFor="role" className="block mb-2 text-sm font-medium text-green-800">Custodian Role</label>
+                                <div className="relative inline-block w-full">
+                                    <button type="button" onClick={() => setShowDropdown(!showDropdown)} className="text-white bg-green-900 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-left inline-flex justify-between items-center w-full">
+                                        {custodian.role || "Select role"}
+                                        <svg className="w-2.5 h-2.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+                                        </svg>
+                                    </button>
+
+                                    {showDropdown && (
+                                        <div className="absolute z-10 mt-2 w-full bg-white divide-y divide-gray-100 rounded-lg shadow">
+                                            <ul className="py-2 text-sm text-green-900">
+                                                {["Supervisor", "Janitor III", "Janitor II"].map((role) => (
+                                                    <li key={role}>
+                                                        <button type="button" onClick={() => {
+                                                            setCustodian({ ...custodian, role });
+                                                            setShowDropdown(false);
+                                                        }}
+                                                            className="w-full text-left px-4 py-2 hover:bg-gray-100"
+                                                        >
+                                                            {role}
+                                                        </button>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
+                            {(custodian.role === "Janitor II" || custodian.role === "Janitor III") && (
+                                <div>
+                                    <label htmlFor="custodianboss" className="block mb-2 text-sm font-medium text-green-800">Custodian's Boss ID</label>
+                                    <input type="text" name="custodianboss" id="custodianboss" value={custodian.name || ""} onChange={(e) => setCustodian({ ...custodian, name: e.target.value })} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Full Name" />
+                                </div>
+                            )}
                             <button type="submit" className="w-full text-white bg-green-700 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:cursor-progress disabled:bg-red-500 transition-colors duration-200">Add Custodian</button>
 
                             <a href="/" className="font-medium text-green-800 text-sm block pt-1 hover:underline">Back to home</a>
