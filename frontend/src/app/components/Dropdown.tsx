f6c2dd64d013fdf6863e40ea1206a18d4512a265
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2026-03-16 19:10:25 -1000

    Dropdown with icon pau (1st part)

--- frontend/src/app/components/Dropdown.tsx
+++ frontend/src/app/components/Dropdown.tsx
@@ -1,21 +1,31 @@
-import { ChevronUpCircle, ChevronDownCircle, Search } from 'lucide-react';
+import { LucideProps } from 'lucide-react';
+import { ReactElement } from 'react';
 
-type dropdownProps = {
+type DropdownProps = {
     id: string,
     name: string,
     values: string[],
     className: string,
+    icon: ReactElement<LucideProps>,
 };
 
-export const Dropdown = ({id, name, values, className}:dropdownProps) => {
+export const Dropdown = ({id, name, values, className, icon}:DropdownProps) => {
     
     return(
-        <div className={`${className} relative`}>
+        <div className={`${className}`}>
             <label htmlFor={name}>{name}</label>
-            <input
-                id={`search_${id}_box`}
-                className="border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-            />
+            <div className="flex items-center border border-gray-300 rounded-lg focus-within:outline overflow-hidden">
+                <div className="text-gray-500 bg-gray-300 flex self-stretch items-center justify-center w-10">
+                    {icon}
+                </div>
+
+                {/* Input field (border removed so the container acts as the border) */}
+                <input
+                    id={id}
+                    type="text"
+                    className="p-2.5 outline-none text-gray-900 bg-transparent"
+                />
+            </div>
         </div>
     );
 }
